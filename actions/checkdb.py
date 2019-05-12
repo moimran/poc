a4f354e590c861a5c9bb902b2a4784a8f6e168bb
Ismail Eltahawy (AU) <ismail.eltahawy@dimensiondata.com>
2019-05-12 14:41:44 +1000

    added check DB

--- actions/checkdb.py
+++ actions/checkdb.py
@@ -4,7 +4,7 @@ import mysql.connector
 class RunCheckPort(Action):
     def run(self, device_ip, port, db_username, db_password):
         print("Attempting to connect to {} on port {}\n".format(device_ip, port))
-        if(self.check_db(device_ip, port, db_username, db_password)):
+        if(self.check_db(device_ip, port, db_username, db_password) == True):
             return True
         else:
             return False
@@ -19,9 +19,10 @@ class RunCheckPort(Action):
             passwd=db_password,
             auth_plugin="mysql_native_password")
             print("Connection to {} on port {} successful".format(device_ip, port))
+            return True
         except:
             print("Connection to {} on port {} failed".format(device_ip, port))
-            return False, 'FAIL'
+            return False
 
 
 
